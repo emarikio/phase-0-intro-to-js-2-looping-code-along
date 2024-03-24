9d37d1d0e6147ce37f8b371562ea9404e408b3c9
Emarilyne Wakio <emarilynew@gmail.com>
2024-03-24 18:12:43 +0300

    looping labs

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, birthday){
+    let cards = [];
+    for (let i = 0; i < names.length; i++) {
+        cards.push (`Thank you, ${names[i]}, for the wonderful ${birthday} gift!`)
+    }
+    return cards;
+}
+function countDown(number) {
+    let count = number;
+    while (count >= 0) {
+        console.log(count);
+        count--;
+    }
+}
